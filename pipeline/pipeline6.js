abd42607720d4f08c1d92579e4828fd4b5abf558
iki1uc <entertrainer@web.de>
2026-07-12 09:21:18 +0200

    Update pipeline6.js

--- pipeline/pipeline6.js
+++ pipeline/pipeline6.js
@@ -1,1 +1,31 @@
+export async function pipeline6() {
+    const out = document.getElementById("pipeline6");
+
+    const respo = [
+        "REAL", "BIL", "DU", "NG"
+    ];
+
+    for (let i = 0; i < respo.length; i++) {
+        const url = `https://iki1uc.github.io/RESPO-${respo[i]}/`;
+
+        try {
+            const res = await fetch(url);
+            out.innerHTML += `
+                <div class="step">
+                    Pipeline‑6: RESPO ${respo[i]}<br>
+                    URL: ${url}<br>
+                    STATUS: ${res.ok ? "ONLINE ✓" : "ERROR ✗"} (${res.status})
+                </div>
+            `;
+        } catch (err) {
+            out.innerHTML += `
+                <div class="step">
+                    Pipeline‑6: RESPO ${respo[i]}<br>
+                    URL: ${url}<br>
+                    EXCEPTION ✗<br>${err.message}
+                </div>
+            `;
+        }
+    }
+}
 
